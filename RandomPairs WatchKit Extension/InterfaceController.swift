47ae4a08c7b4446247ac5a28ad67d3b394a829f0
davidrossouw <davidrossouw@gmail.com>
2021-02-27 21:14:34 -0500

    working app

--- RandomPairs WatchKit Extension/InterfaceController.swift
+++ RandomPairs WatchKit Extension/InterfaceController.swift
@@ -12,11 +12,33 @@ import Foundation
 
 class InterfaceController: WKInterfaceController {
 
+//    var sliderValue:Float = 1.0
+    
+    @IBOutlet weak var slider: WKInterfaceSlider!
+    
+    @IBOutlet weak var label: WKInterfaceLabel!
+    
+    @IBOutlet weak var assignmentLabel: WKInterfaceLabel!
+    
+    @IBAction func sliderDidChange(_ value: Float) {
+        print("value: ", value)
+        label.setText("Players: \(Int(value))")
+        let sequence = 1...Int(value)
+        let shuffled = sequence.shuffled()
+        var output = [String]()
+        for i in stride(from: 0, to: shuffled.count - 1, by: 2) {
+            output.append(String(shuffled[i])+" v "+String(shuffled[i+1]))
+        }
+        //assignmentLabel.li = output.count
+        assignmentLabel.setText(output.joined(separator: "\n"))
+        print(output.joined(separator: "\n"))
+    }
+      
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
-        
         // Configure interface objects here.
     }
+
     
     override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
